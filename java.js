7415cbef32b8583e4c789710bd4630b1c966d429
Rakshith-Raj08 <rakshith2002raj@gmail.com>
2023-06-23 00:05:45 +0530

    Added Speaker Icon and audio

---
+++ java.js
@@ -0,0 +1,16 @@
+var buttonImage = document.getElementById("volume");
+
+var audio = new Audio("Assests/Music/The Batman (2022) ! Theme ! Bgm.mp3");
+
+buttonImage.addEventListener("click", function() {
+  if (buttonImage.src.includes("Assests/Images/Volume.png")) {
+    buttonImage.src = "Assests/Images/Silent.png";
+    audio.pause();
+  } else {
+    buttonImage.src = "Assests/Images/Volume.png";
+    audio.play();
+    audio.loop = true;
+  }
+});
+
+
